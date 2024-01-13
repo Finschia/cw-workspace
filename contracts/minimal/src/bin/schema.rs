a1516d5c5b096aa17080316611c150e97649a1f0
Shogo Hyodo <mmoshg8u@gmail.com>
2024-01-13 15:25:50 +0900

    Fix schema.rs

--- contracts/minimal/src/bin/schema.rs
+++ contracts/minimal/src/bin/schema.rs
@@ -1,6 +1,6 @@
 use cosmwasm_schema::write_api;
 
-use minimal::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
+use {{crate_name}}::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
 
 fn main() {
     write_api! {
